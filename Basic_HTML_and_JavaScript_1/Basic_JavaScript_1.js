4089dbb943b99c408c095c38dbf446b931815bdb
Mila Nevedomskaya <milo4kamoya@gmail.com>
2023-03-21 14:43:56 -0700

    Basic HTML and JavaScript 1 Submission Assignment

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("Hello, World!!!");
+
+function My_First_Function() {
+	let String = "Kiss me, I'm Irish!";
+	let result = String.fontcolor("green");
+	document.getElementById("Irish").innerHTML = result;
+}
